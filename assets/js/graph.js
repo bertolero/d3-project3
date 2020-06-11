74684de02b8297dd1ba21159b505b350a89b9ad7
Bertol <bertol.pinheiro@gmail.com>
2020-06-11 18:04:24 -0300

    Add base graph setup.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -2,3 +2,42 @@ import * as d3Base from "d3";
 import db from "./data/firebase-utils";
 
 const d3 = Object.assign(d3Base);
+let data = [];
+
+const dims = { height: 500, width: 1100 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 100)
+  .attr("height", dims.height + 100);
+
+const graph = svg.append("g").attr("transform", "translate(50,50)");
+
+// react to changes on firestore
+db.collection("employees").onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+
+  update(data);
+});
+
+const update = (data) => {
+  console.log(data);
+};
