8581be1bb9154327c4f4636d00aa48e0da2e94da
Bertol <bertol.pinheiro@gmail.com>
2020-06-11 18:49:40 -0300

    Add captions and lines.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -14,6 +14,14 @@ const svg = d3
 
 const graph = svg.append("g").attr("transform", "translate(50,50)");
 
+// data stratify
+const stratify = d3
+  .stratify()
+  .id((d) => d.name)
+  .parentId((d) => d.parent);
+
+const tree = d3.tree().size([dims.width, dims.height]);
+
 // react to changes on firestore
 db.collection("employees").onSnapshot((res) => {
   res.docChanges().forEach((change) => {
@@ -39,5 +47,52 @@ db.collection("employees").onSnapshot((res) => {
 });
 
 const update = (data) => {
-  console.log(data);
+  // get root node
+  const rootNode = stratify(data);
+
+  const treeData = tree(rootNode);
+
+  // get node selection
+  const nodes = graph.selectAll(".node").data(treeData.descendants());
+
+  // get link selection and join data
+  const links = graph.selectAll(".link").data(treeData.links());
+
+  //enter new links
+  links
+    .enter()
+    .append("path")
+    .attr("class", "link")
+    .attr("fill", "none")
+    .attr("stroke", "#aaa")
+    .attr("stroke-width", 2)
+    .attr(
+      "d",
+      d3
+        .linkVertical()
+        .x((d) => d.x)
+        .y((d) => d.y)
+    );
+
+  // create enter node groups
+  const enterNodes = nodes
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", (d) => `translate(${d.x},${d.y})`);
+
+  // append rects to enter nodes
+  enterNodes
+    .append("rect")
+    .attr("fill", "#aaa")
+    .attr("stroke", "#555")
+    .attr("stroke-width", 2)
+    .attr("height", 50)
+    .attr("width", (d) => d.data.name.length * 20);
+
+  enterNodes
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text((d) => d.data.name);
 };
