06930150c42153ea43c67912bd99f0e0e46682a1
Bertol <bertol.pinheiro@gmail.com>
2020-06-11 17:53:35 -0300

    Add form actions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,30 @@
+import db from "./data/firebase-utils";
+
 const modal = document.querySelector(".modal");
 M.Modal.init(modal);
+
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const parent = document.querySelector("#parent");
+const department = document.querySelector("#department");
+
+form.addEventListener("submit", (ev) => {
+  console.log(ev);
+
+  ev.preventDefault();
+
+  db.collection("employees")
+    .add({
+      name: name.value,
+      parent: parent.value,
+      department: department.value,
+    })
+    .then(() => {
+      let instance = M.Modal.getInstance(modal);
+      instance.close();
+      form.reset();
+    })
+    .catch((onerror) => {
+      console.error(onerror);
+    });
+});
